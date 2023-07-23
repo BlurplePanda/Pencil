764a035f1c2596c30d59e7a989214fc3de4822ae
BlurplePanda <bootham@wgc.school.nz>
2023-07-24 11:48:54 +1200

    Added redo (and empty stack checks)

--- Pencil.java
+++ Pencil.java
@@ -23,6 +23,7 @@ public class Pencil{
     public void setupGUI(){
         UI.setMouseMotionListener(this::doMouse);
         UI.addButton("Undo", this::undo);
+        UI.addButton("Redo", this::redo);
         UI.addButton("Quit", UI::quit);
         UI.setLineWidth(3);
         UI.setDivider(0.0);
@@ -42,6 +43,7 @@ public class Pencil{
         else if (action.equals("released")){
             stroke.draw();
             drawn.push(stroke);
+            undone.clear();
         }
     }
 
@@ -53,9 +55,19 @@ public class Pencil{
     }
 
     public void undo() {
-        Stroke removed = drawn.pop();
-        undone.push(removed);
-        display();
+        if (!drawn.isEmpty()) {
+            Stroke toUndo = drawn.pop();
+            undone.push(toUndo);
+            display();
+        }
+    }
+
+    public void redo() {
+        if (!undone.isEmpty()) {
+            Stroke toRedo = undone.pop();
+            drawn.push(toRedo);
+            display();
+        }
     }
 
     public static void main(String[] arguments){
